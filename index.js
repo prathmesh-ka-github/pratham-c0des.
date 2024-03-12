724b0153964c2978b9331f1e203a928a69b60b54
Prathmesh Kale <connect.prathmesh905@gmail.com>
2024-03-12 19:22:28 +0530

    color and font changes, nothing major

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function ham() {
     var menu = document.getElementById("ham-menu-id");
     if (menu.style.opacity === '1'){
         menu.style.opacity = '0';
-        menu.style.top = '-255px';
+        menu.style.top = '-300px';
     }
     else {
         menu.style.opacity = '1';
