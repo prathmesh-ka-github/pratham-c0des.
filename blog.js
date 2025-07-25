f27ddef4dcb1819fc9c890810f9ad8b9bbab3f21
prathmesh-ka-github <pkale@albany.edu>
2025-07-25 11:18:30 -0400

    :sparkles: added get views functionality

--- blog.js
+++ blog.js
@@ -1,6 +1,6 @@
 // import { getViews } from "./viewsAPI"
 
-function showblog(id){
+async function showblog(id){
     if (document.getElementById(`blogt${id}`)) {
         blogtid = document.getElementById(`blogt${id}`)
         blogtid.classList.toggle('big-title')
@@ -19,4 +19,43 @@ function showblog(id){
             blogimg.classList.toggle('showimg')
         });
     }
+    // document.cookie = "blog1=true; expires=Thu, 17 Dec 2025 12:00:00 UTC; path=/";
+    // document.cookie = "username=John Smith; expires=Thu, 18 Dec 2033 12:00:00 UTC; path=/";
+    // document.cookie = "name=Tim Cook; expires=Thu, 18 Dec 2033 12:00:00 UTC; path=/";
+    // document.cookie = "name; expires=Thu, 18 Dec 2033 12:00:00 UTC; path=/";
+    // const now = new Date();
+    // console.log(now.getHours());
+    // let allcookies = decodeURIComponent(document.cookie);
+    // let cookiearry = allcookies.split(';')
+    // console.log(allcookies);
+    
+    // fetch('http://localhost:3000/blogviews').then( res => {
+    //     if (!res.ok) {
+    //         throw new Error(`HTTP error! status: ${response.status}`);
+    //     }
+    //     return res.json()
+    // }).then( data => {
+    //     data.forEach(d => {
+    //         if (d.blog == id) {
+    //             console.log(d)
+    //         }
+    //     })
+    //     console.log(data)
+    // })
 }
+
+async function getViews(){
+    fetch('http://localhost:3000/blogviews').then( res => {
+        if (!res.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return res.json()
+    }).then( data => {
+        data.forEach(d => {
+            const displayview = document.getElementById(`view${d.blog}`)
+            displayview.innerText = d.views
+        })
+    })
+    // view1
+}
+getViews()
